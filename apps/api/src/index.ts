ae6d0d512e964f92f20e0d26c87d61200abdd5e5
waynemwendwa <waynemwendwa04@gmail.com>
2025-09-13 13:48:22 +0300

    fixed api deployment error

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -17,8 +17,6 @@ app.use(cors({
 	allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
-// Handle preflight requests
-app.options('*', cors());
 app.use(express.json());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
